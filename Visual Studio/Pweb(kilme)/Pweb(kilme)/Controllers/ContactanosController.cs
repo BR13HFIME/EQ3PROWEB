0190e0e54ccbb71ccbc43325fee955d04b9dc217
Clarissa Reyna <clarissareynagzz@gmail.com>
2024-11-25 00:41:42 -0600

    prueba

---
+++ Visual Studio/Pweb(kilme)/Pweb(kilme)/Controllers/ContactanosController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Pweb_kilme_.Controllers
+{
+    public class ContactanosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
