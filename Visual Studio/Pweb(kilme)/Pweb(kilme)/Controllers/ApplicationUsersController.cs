b31e79803ed04fbd2aeb870fd04d416e26fadaab
BR13HFIME <brian.reynadzd@uanl.edu.mx>
2024-11-25 21:47:10 -0600

    Sigue sin funcionar el create de datosreservacion, ayuda

--- Visual Studio/Pweb(kilme)/Pweb(kilme)/Controllers/ApplicationUsersController.cs
+++ Visual Studio/Pweb(kilme)/Pweb(kilme)/Controllers/ApplicationUsersController.cs
@@ -6,24 +6,20 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Pweb_kilme_.Models.dbModels;
-
 namespace Pweb_kilme_.Controllers
 {
     public class ApplicationUsersController : Controller
     {
         private readonly PwebdbContext _context;
-
         public ApplicationUsersController(PwebdbContext context)
         {
             _context = context;
         }
-
         // GET: ApplicationUsers
         public async Task<IActionResult> Index()
         {
             return View(await _context.Users.ToListAsync());
         }
-
         // GET: ApplicationUsers/Details/5
         public async Task<IActionResult> Details(int? id)
         {
@@ -31,25 +27,19 @@ namespace Pweb_kilme_.Controllers
             {
                 return NotFound();
             }
-
-            var datosreservacion = await _context.Datosreservacions
-                .Include(d => d.IdEstadoNavigation)
-                .Include(d => d.IdQuintaNavigation)
-                .Include(d => d.IdUsuarioNavigation)
-                .FirstOrDefaultAsync(m => m.IdReservacion == id);
-            if (datosreservacion == null)
+            var applicationUser = await _context.Users
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (applicationUser == null)
             {
                 return NotFound();
             }
-
+            return View(applicationUser);
         }
-
         // GET: ApplicationUsers/Create
         public IActionResult Create()
         {
             return View();
         }
-
         // POST: ApplicationUsers/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
@@ -65,7 +55,6 @@ namespace Pweb_kilme_.Controllers
             }
             return View(applicationUser);
         }
-
         // GET: ApplicationUsers/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
@@ -73,7 +62,6 @@ namespace Pweb_kilme_.Controllers
             {
                 return NotFound();
             }
-
             var applicationUser = await _context.Users.FindAsync(id);
             if (applicationUser == null)
             {
@@ -81,7 +69,6 @@ namespace Pweb_kilme_.Controllers
             }
             return View(applicationUser);
         }
-
         // POST: ApplicationUsers/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
@@ -93,7 +80,6 @@ namespace Pweb_kilme_.Controllers
             {
                 return NotFound();
             }
-
             if (ModelState.IsValid)
             {
                 try
@@ -116,7 +102,6 @@ namespace Pweb_kilme_.Controllers
             }
             return View(applicationUser);
         }
-
         // GET: ApplicationUsers/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
@@ -124,17 +109,14 @@ namespace Pweb_kilme_.Controllers
             {
                 return NotFound();
             }
-
             var applicationUser = await _context.Users
                 .FirstOrDefaultAsync(m => m.Id == id);
             if (applicationUser == null)
             {
                 return NotFound();
             }
-
             return View(applicationUser);
         }
-
         // POST: ApplicationUsers/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
@@ -145,11 +127,9 @@ namespace Pweb_kilme_.Controllers
             {
                 _context.Users.Remove(applicationUser);
             }
-
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
-
         private bool ApplicationUserExists(int id)
         {
             return _context.Users.Any(e => e.Id == id);
